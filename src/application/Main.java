11fb1c51cc9d5c5c827abd12cf6e0b86160602a2
AwitSandoy <awit.sandoy@gmail.com>
2026-07-20 02:53:07 +0800

    Add files via upload

---
+++ src/application/Main.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/LoginRegister.fxml"));
+        primaryStage.setTitle("Smart Parking Management System");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMinWidth(500);
+        primaryStage.setMinHeight(500);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
